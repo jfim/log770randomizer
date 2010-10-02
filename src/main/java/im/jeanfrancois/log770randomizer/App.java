0cd8d757321083d7ea5962734af40f5dbfe4ec47
Jean-Francois Im <jeanfrancois.im@gmail.com>
2010-10-02 16:42:59 -0400

    Ajout du code permettant de demarrer l'application

--- src/main/java/im/jeanfrancois/log770randomizer/App.java
+++ src/main/java/im/jeanfrancois/log770randomizer/App.java
@@ -1,12 +1,24 @@
 package im.jeanfrancois.log770randomizer;
 
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
 /**
- * Hello world!
+ * Entry point for the LOG770 randomizer
  *
+ * @author jfim
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		WekaRandomizer wekaRandomizer = new WekaRandomizer();
+		CmdLineParser cmdLineParser = new CmdLineParser(wekaRandomizer);
+
+		try {
+			cmdLineParser.parseArgument(args);
+			wekaRandomizer.run();
+		} catch (CmdLineException e) {
+			System.err.println(e.getMessage());
+			cmdLineParser.printUsage(System.err);
+		}
+	}
 }
